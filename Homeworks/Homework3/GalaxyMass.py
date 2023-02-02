9d4d3cd2f82a41c90135ad1404fbff4ce0d88743
jaymotka <jaymotka@arizona.edu>
2023-02-02 09:41:46 -0700

    Uploading the final code for HW3.

--- Homeworks/Homework3/GalaxyMass.py
+++ Homeworks/Homework3/GalaxyMass.py
@@ -1,4 +1,6 @@
 from ReadFile import Read
+import numpy as np
+import astropy.units as u
 
 def ComponentMass(filename, part_type):
 	"""This function finds the total mass of any desired galaxy component.
@@ -18,8 +20,32 @@ def ComponentMass(filename, part_type):
 		Total mass of the desired galaxy component in 10^12 M_Sun.
 	"""
 
-	time, total_part, data = Read(filename)
+	time, total_part, data = Read(filename)                 # Calling Read() from ReadFile.py
 
-	print(data[0:2])
+	index = np.where(data['type']==part_type)               # Finding indexes of the given type of particles.
+	mass_data = data['m'][index]                            # Making an array of masses of the desired type of particles.
 
-ComponentMass('~/mystuff/ASTR_400B/400B_2023_motka/Data/MW_000.txt', 1)
+	mass = np.sum(mass_data)*1e+10*u.Msun                   # Taking the sum of masses of the desired type of particles.
+
+	mass = np.round(mass.to(1e+12*u.Msun), decimals=3)      # Converting to the units of 10^12 M_Sun and rounding to 3 decimal digits.
+
+	return mass
+
+
+# Testing:
+#table = []
+#for file in ['MW_000.txt', 'M31_000.txt', 'M33_000.txt']:
+#	row = [file.strip('_000.txt')]
+#	total_mass = 0
+#	stellar_mass = 0
+#	for i in range(1,4):
+#		mass = ComponentMass('/home/jaymotka/400B_2023_motka/Data/' + file, i)
+#		row.append(mass)
+#		total_mass += mass
+#		if i != 1:
+#			stellar_mass += mass
+#	row.append(total_mass)
+#	row.append(np.round(stellar_mass/total_mass, decimals=3))
+#	table.append(row)
+#table = np.array(table)
+#print(table)
